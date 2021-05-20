edd6c7b49d12f926ef46f21f207d3415380018c2
Mayank0307 <mayank141236@gmail.com>
2021-05-20 09:27:07 +0530

    final commit

---
+++ src/components/home/Home.js
@@ -0,0 +1,38 @@
+import React, { useEffect, Fragment } from "react";
+import { connect } from "react-redux";
+import { getStateData } from "../../store/actions/states";
+import State from "./State/State";
+import DisplayChart from "./DisplayChart";
+
+const Home = (props) => {
+  useEffect(() => {
+    props.getStateData();
+  }, []);
+
+  return (
+    <div className="container">
+      <DisplayChart
+        stateData={props.stateData
+          .sort(function (a, b) {
+            return b.confirmed - a.confirmed;
+          })
+          .slice(1, 11)}
+        nationalTimeline={props.nationalTimeline}
+      />
+      <State stateData={props.stateData.slice(1)} />
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  stateData: state.states.stateData,
+  nationalTimeline: state.states.countryTimeline,
+  loaded: state.states.loaded,
+  error: state.states.error,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  getStateData: () => dispatch(getStateData()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
