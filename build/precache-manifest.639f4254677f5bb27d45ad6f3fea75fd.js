62bf63e6f66135418e07482e442c62fce4a24415
Mayank Gupta <52233220+Mayank0307@users.noreply.github.com>
2021-05-20 10:46:37 +0530

    Add files via upload

---
+++ build/precache-manifest.639f4254677f5bb27d45ad6f3fea75fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "722e93bb1da27fff40f0f3f6206c5e39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22fa1875c285564d9c34",
+    "url": "/static/css/main.3f4e4d86.chunk.css"
+  },
+  {
+    "revision": "e5f4148a9fe16bb29fa2",
+    "url": "/static/js/2.252a51a0.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/static/js/2.252a51a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22fa1875c285564d9c34",
+    "url": "/static/js/main.1431d501.chunk.js"
+  },
+  {
+    "revision": "58a60eb180a36f5c2310",
+    "url": "/static/js/runtime-main.569ab1a9.js"
+  }
+]);
